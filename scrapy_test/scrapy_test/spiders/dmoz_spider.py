e4c4215b37a5487af7a726b2dfeecea0caa85afe
“ygongwenqiang2git config --global user.email “526413511@qq.com <526413511@qq.com>
2016-10-11 16:49:59 +0800

    eeee

--- scrapy_test/scrapy_test/spiders/dmoz_spider.py
+++ scrapy_test/scrapy_test/spiders/dmoz_spider.py
@@ -1,4 +1,3 @@
-# -*- coding:utf-8 -*-
 import scrapy
 
 class DmozSpider(scrapy.spiders.Spider):
@@ -10,6 +9,8 @@ class DmozSpider(scrapy.spiders.Spider):
     ]
 
     def parse(self, response):
-        filename = response.url.split("/")[-2]
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+        for sel in response.xpath('//ul/li'):
+            title = sel.xpath('a/text()').extract()
+            link = sel.xpath('a/@href').extract()
+            desc = sel.xpath('text()').extract()
+            print title, link, desc
