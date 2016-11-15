2241bf3068b9943df24c7dd60c8e137ba9d6da7f
unknown <>
2016-11-15 23:38:21 +0800

    20161115pm

--- data_mining_learn/clustering/test01.py
+++ data_mining_learn/clustering/test01.py
@@ -1,42 +1,28 @@
-
+#-*- coding: utf-8 -*-
 # Authors: Gael Varoquaux, Nelle Varoquaux
 # License: BSD 3 clause
 
-import numpy as np
-from sklearn.datasets import make_blobs
-from sklearn.preprocessing import StandardScaler
 
-from sklearn.cluster import DBSCAN
-from sklearn.preprocessing import StandardScaler
-import matplotlib.pyplot as plt
+from flask import Flask,request,render_template
 
-centers=[[1,1],[-1,-1],[1,-1]]
-X,real_clusters=make_blobs(n_samples=750,centers=centers,cluster_std=0.4,random_state=0)
-X=StandardScaler().fit_transform(X)
-
-db=DBSCAN(eps=0.3,min_samples=10).fit(X)
-core_index=db.core_sample_indices_
-is_core=np.zeros(len(X),dtype=bool)
-is_core[core_index]=True
-
-labels=db.labels_
-n_clusters=len(np.unique(labels))-(1 if -1 in labels else 0)
-colors=['b','g','r','c','m','y']
-colors=colors[:(n_clusters+1)]
-unique_labels=set(labels)
-colors = plt.cm.Spectral(np.linspace(0, 1, len(unique_labels)))
-
-for label,color in zip(unique_labels,colors):
-    if label==-1:
-        color='k'
-
-    class_member_mask = (labels == label)
-
-    xy_core=X[class_member_mask & is_core]
-    plt.plot(xy_core[:,0],xy_core[:,1],'o',markerfacecolor=color,markeredgecolor='k',markersize=14)
-
-    xy_other=X[class_member_mask & ~is_core]
-    plt.plot(xy_other[:,0],xy_other[:,1],'o',markerfacecolor=color,markeredgecolor='k',markersize=6)
+app=Flask(__name__)
 
 
-plt.show()
+@app.route('/',methods=['GET','POST'])
+def home():
+    return render_template('home.html')
+
+@app.route('/signin',methods=['GET'])
+def signin_form():
+    return render_template('form.html')
+
+@app.route('/signin',methods=['POST'])
+def sign():
+    username=request.form['username']
+    password=request.form['password']
+    if uasername=='username' and password=='password':
+        return render_template('signin.html',username=username)
+    return render_template('form.html', message='Bad username or password', username=username)
+
+if __name__=='__main__':
+    app.run()
